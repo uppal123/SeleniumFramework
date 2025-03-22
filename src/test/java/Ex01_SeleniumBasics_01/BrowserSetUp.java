32c80619754d1cac9592ec0c2a29134f16bfc848
poojauppal <pujapuja35@gmail.com>
2025-03-23 01:44:01 +0530

    part 1

--- src/test/java/Ex01_SeleniumBasics_01/BrowserSetUp.java
+++ src/test/java/Ex01_SeleniumBasics_01/BrowserSetUp.java
@@ -1,4 +1,14 @@
 package Ex01_SeleniumBasics_01;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class BrowserSetUp {
+    @Test
+    public void test_VerifyVWOSetUp() {
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://app.vwo.com/#/login");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getSessionId());
+    }
 }
